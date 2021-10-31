9fd1fa5846ac93f0ac723c850eefaa8e708b93e4
Dien Chau <dienkchau@gmail.com>
2021-10-31 14:56:25 -0500

    Put main code in function

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 try:
     import pygame
+    import sys
 except ImportError as e:
     print("There was an import error: " + e)
 
-if __name__ == "__main__":
+def main():
     # Initialize Screen
     WIDTH, HEIGHT = 1200, 600
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("3D Rendering")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    finally:
+        sys.exit()
