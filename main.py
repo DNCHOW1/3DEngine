f02e031f7fea7e2e1d6823761e5a4de53803a536
Dien Chau <dienkchau@gmail.com>
2021-10-31 14:45:13 -0500

    Initialized screen

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ try:
     import pygame
 except ImportError as e:
     print("There was an import error: " + e)
+
+if __name__ == "__main__":
+    # Initialize Screen
+    WIDTH, HEIGHT = 1200, 600
+    pygame.init()
+    screen = pygame.display.set_mode(WIDTH, HEIGHT)
+    pygame.display.set_caption("3D Rendering")
